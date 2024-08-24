bb1a4addf034a745fab1f862d15691c6aad2e14b
joaowoigt <joaowoigt@gmail.com>
2024-08-24 11:26:03 -0300

    feat list

--- pages/index.js
+++ pages/index.js
@@ -3,57 +3,61 @@ import CursiveTitle from "../src/CursiveTitle"
 import HomeMenu from "../src/homeMenu"
 import container from "../src/components/Container.module.css"
 import fonts from "../src/components/Font.module.css"
-import { useTheme } from "next-themes"
+import { ChakraProvider } from "@chakra-ui/react"
+import { theme } from "@/theme"
 
 export default function HomePage() {
-  const { theme, setTheme } = useTheme()
-  setTheme("light")
   return (
-    <div className={container.centerDiv}>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Playwrite+CU:wght@100..400&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <div className={container.mainContainer}>
-        <HomeMenu></HomeMenu>
-        <CursiveTitle>João & Milleny</CursiveTitle>
-        <img
-          className={container.imgContainer}
-          src="images\foto-nossa.jpg"
-        ></img>
-        <h4 className={fonts.latoRegular}>
-          É com muita alegria que nós, Milleny e João, anunciamos a nossa união
-          eterna. <br></br>No dia{" "}
-          <b>05 de Julho de 2025 às 15h30, no espaço Colibri em Limeira - SP</b>
-          , diante de nossos amigos e familiares, gostariamos de celebrar o
-          nosso amor. <br></br> Neste site você pode acessar nossa lista de
-          presentes e o formulário de confirmação de presença.
-        </h4>
-        <h5 className={fonts.latoRegular}>
-          Qualquer duvida você pode nos contatar nos telefones:<br></br>
-          <b>João: (19) 98826-0505</b>
-          <br></br>
-          <b>Milleny: (19) 97131-6619</b>
-          <br></br>
-          <br></br>
-          Endereço da festa:
-          <br></br>
-          <b>
-            Rua Servidão 1, 299-627 - Parque das Nações, Limeira - SP, 13480-970
-          </b>
-          <br></br>
-          Ou acesse a localização{" "}
-          <b>
-            <a href="https://www.google.com/maps/place/Espa%C3%A7o+Colibri/@-22.5869195,-47.4192071,16z/data=!4m10!1m2!2m1!1sColibri+limeira!3m6!1s0x94c881aa7137088f:0x2510e69c03cf6091!8m2!3d-22.5869195!4d-47.4101949!15sCg9Db2xpYnJpIGxpbWVpcmGSAQ13ZWRkaW5nX3ZlbnVl4AEA!16s%2Fg%2F11g6rl0k7z?entry=ttu">
-              clicando aqui
-            </a>
-          </b>
-        </h5>
+    <ChakraProvider theme={theme}>
+      <div className={container.centerDiv}>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Playwrite+CU:wght@100..400&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <div className={container.mainContainer}>
+          <HomeMenu></HomeMenu>
+          <CursiveTitle>João & Milleny</CursiveTitle>
+          <img
+            className={container.imgContainer}
+            src="images\foto-nossa.jpg"
+          ></img>
+          <h4 className={fonts.latoRegular}>
+            É com muita alegria que nós, Milleny e João, anunciamos a nossa
+            união eterna. <br></br>No dia{" "}
+            <b>
+              05 de Julho de 2025 às 15h30, no espaço Colibri em Limeira - SP
+            </b>
+            , diante de nossos amigos e familiares, gostariamos de celebrar o
+            nosso amor. <br></br> Neste site você pode acessar nossa lista de
+            presentes e o formulário de confirmação de presença.
+          </h4>
+          <h5 className={fonts.latoRegular}>
+            Qualquer duvida você pode nos contatar nos telefones:<br></br>
+            <b>João: (19) 98826-0505</b>
+            <br></br>
+            <b>Milleny: (19) 97131-6619</b>
+            <br></br>
+            <br></br>
+            Endereço da festa:
+            <br></br>
+            <b>
+              Rua Servidão 1, 299-627 - Parque das Nações, Limeira - SP,
+              13480-970
+            </b>
+            <br></br>
+            Ou acesse a localização{" "}
+            <b>
+              <a href="https://www.google.com/maps/place/Espa%C3%A7o+Colibri/@-22.5869195,-47.4192071,16z/data=!4m10!1m2!2m1!1sColibri+limeira!3m6!1s0x94c881aa7137088f:0x2510e69c03cf6091!8m2!3d-22.5869195!4d-47.4101949!15sCg9Db2xpYnJpIGxpbWVpcmGSAQ13ZWRkaW5nX3ZlbnVl4AEA!16s%2Fg%2F11g6rl0k7z?entry=ttu">
+                clicando aqui
+              </a>
+            </b>
+          </h5>
+        </div>
       </div>
-    </div>
+    </ChakraProvider>
   )
 }
--- pages/lista.js
+++ pages/lista.js
@@ -20,30 +20,50 @@ export default function ListaPage() {
             a sua contribuição.
             <br></br>Abaixo{" "}
             <b>você encontra o QRcode para realizar o pix para nossa conta.</b>{" "}
-            Sinta-se a vontade para contribuir como puder.
+            Desta forma, você pode escolher algum presente da lista abaixo e
+            mandar o pix referente ao valor, ou sinta-se a vontade para
+            contribuir como puder.
           </h4>
           <img
             className={containers.imgPixContainer}
             src="images\pix-qr-code.png"
           ></img>
           <div className={containers.listGridItem}>
-            <ListItem img="\images\avatar.jpg" value="R$ 50,00">
-              Whiskas para nossos gatos
+            <ListItem img="\images\whiskas.jpg" value="R$ 50,00">
+              Pacotes de Whiskas para nossos gatos
             </ListItem>
-            <ListItem img="\images\avatar.jpg" value="R$ 1000,00">
-              Texto de descrição do presente
+            <ListItem img="\images\rp.webp" value="R$ 100,00">
+              Cartão pré-pago de RP para o João
             </ListItem>
-            <ListItem img="\images\avatar.jpg" value="R$ 1000,00">
-              Texto de descrição do presente
+            <ListItem img="\images\restaurante.jpg" value="R$ 200,00">
+              Jantar romantico na lua de mel
             </ListItem>
-            <ListItem img="\images\avatar.jpg" value="R$ 1000,00">
-              Texto de descrição do presente
+            <ListItem img="\images\esteto.webp" value="R$ 300,00">
+              Estetoscópio para a Dra. Milleny
             </ListItem>
-            <ListItem img="\images\avatar.jpg" value="R$ 1000,00">
-              Texto de descrição do presente
+            <ListItem img="\images\cama.jpg" value="R$ 400,00">
+              Conjunto de cama branco INCRIVEL
             </ListItem>
-            <ListItem img="\images\avatar.jpg" value="R$ 10.000,00">
-              Viagem pra Paris
+            <ListItem img="\images\aspirador.webp" value="R$ 500,00">
+              Aspirador de pó robo
+            </ListItem>
+            <ListItem img="\images\frigobar.png" value="R$ 1.000,00">
+              Frigobar retro sonho de consumo!!!!!
+            </ListItem>
+            <ListItem img="\images\lava-loucas.webp" value="R$ 2.000,00">
+              Lava louças
+            </ListItem>
+            <ListItem img="\images\lava-seca.webp" value="R$ 3.000,00">
+              Maquina Lava e Seca
+            </ListItem>
+            <ListItem img="\images\tv.jpg" value="R$ 4.000,00">
+              TV grande
+            </ListItem>
+            <ListItem img="\images\hotel.jpg" value="R$ 5.000,00">
+              Diária em hotel 5 estrelas para a lua de mel
+            </ListItem>
+            <ListItem img="\images\viagem.png" value="R$ 10.000,00">
+              Passagem para a lua de mel
             </ListItem>
           </div>
         </div>
